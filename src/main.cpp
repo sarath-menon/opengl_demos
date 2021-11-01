eee05d54772721b8a952e7b1919afebfcc1c0db0
smenon <smenon@ethz.ch>
2021-11-01 12:31:09 +0100

    added documentation

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 int main() {
-  // glm test
-  glm::vec4 vec(1.0f, 0.0f, 0.0f, 0.0f);
-  glm::mat4 trans = glm::mat4(1.0f);
-  trans = glm::translate(trans, glm::vec3(1.0f, 2.0f, 0.0f));
+  // // glm test
+  // glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+  // glm::mat4 trans = glm::mat4(1.0f);
+  // trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+  // vec = trans * vec;
+  // std::cout << vec.x << '\t' << vec.y << '\t' << vec.z << std::endl;
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -19,21 +21,25 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-  // Create window
-
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  // Window  Parameters: width, height,title
+  GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+  // Error check for safety
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
+
+  // Make window part of current context
   glfwMakeContextCurrent(window);
 
+  // Load glad with error checking
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
 
+  // Set widnow area for rendering
   glViewport(0, 0, 800, 800);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -43,11 +49,16 @@ int main() {
     // Inputs
     processInput(window);
 
-    // rendering commands here
+    // rendering commands here~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // set colour
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // set back buffer colour
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // check and call events and swap the buffers
+    // Process events~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // swap back buffer to see the front buffer
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
