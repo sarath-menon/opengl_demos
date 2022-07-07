241680e3f932abafbb5615614bb1a3ea2101aea8
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-07 14:30:52 +0530

    triangle demo works

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,24 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 int main() {
-  // // glm test
-  // glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-  // glm::mat4 trans = glm::mat4(1.0f);
-  // trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-  // vec = trans * vec;
-  // std::cout << vec.x << '\t' << vec.y << '\t' << vec.z << std::endl;
 
+  // Initialize GLFW
   glfwInit();
   // Set opengl version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  // use opengl core (modern) profile
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+// macos extras for glfw
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-  // Window  Parameters: width, height,title
+  // Window  Parameters: width, height, windowname
   GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+
   // Error check for safety
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
@@ -44,12 +43,35 @@ int main() {
   glViewport(0, 0, 800, 800);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  // Create Vertex Shader Object and get its reference
+  // Shaders ------------------------------------------------------------
+
+  // Create reference to access vertex shader
   GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  // Attach Vertex Shader source to the Vertex Shader Object
+  // Attach Vertex Shader source to the Vertex Shader Object, 1-> one screen
   glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  // Compile the Vertex Shader into machine code
+  // Compile the Vertex Shader into machine code for GPU
   glCompileShader(vertexShader);
+
+  // Create reference to access fragment shader
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  // Attach Fragment Shader source to the Fragment Shader Object, 1-> one screen
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  // Compile the Fragment Shader into machine code for GPU
+  glCompileShader(fragmentShader);
+
+  // Create Shader Program Object and get its reference
+  GLuint shaderProgram = glCreateProgram();
+  // Attach the Vertex and Fragment Shaders to the Shader Program
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  // Wrap-up/Link all the shaders together into the Shader Program
+  glLinkProgram(shaderProgram);
+
+  // Delete the now useless Vertex and Fragment Shader objects
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // ------------------------------------------------------------------------------
 
   // Vertices coordinates
   GLfloat vertices[] = {
@@ -58,7 +80,35 @@ int main() {
       0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f  // Upper corner
   };
 
-  // Show window. Render loop
+  // Pack data into buffers to send to GPU
+
+  // Create reference containers for the Vartex Array Object and the Vertex
+  // Buffer Object
+  GLuint VAO, VBO;
+
+  // Generate the VAO and VBO with only 1 object each
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  // Make the VAO the current Vertex Array Object by binding it
+  glBindVertexArray(VAO);
+
+  // Bind the VBO specifying it's a GL_ARRAY_BUFFER
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // Introduce the vertices into the VBO
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  // Enable the Vertex Attribute so that OpenGL knows to use it
+  glEnableVertexAttribArray(0);
+
+  // Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO
+  // and VBO we created
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  //  Render loop: show window till close button is pressed
   while (!glfwWindowShouldClose(window)) {
 
     // Inputs
@@ -73,12 +123,26 @@ int main() {
 
     // Process events~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    // swap back buffer to see the front buffer
+    // Tell OpenGL which Shader Program we want to use
+    glUseProgram(shaderProgram);
+    // Bind the VAO so OpenGL knows to use it
+    glBindVertexArray(VAO);
+    // Draw the triangle using the GL_TRIANGLES primitive
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // Swap the back buffer with the front buffer
     glfwSwapBuffers(window);
+
+    // process events, such as close button pressed
     glfwPollEvents();
   }
 
-  // Close window
+  // Delete all the objects we've created
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+  // Delete window before ending the program
+  glfwDestroyWindow(window);
+  // Terminate GLFW before ending the program
   glfwTerminate();
   return 0;
 }
