5073a2513953fda2970e38b62e0164281a566dc2
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-08 12:57:47 +0530

    code cleanup

--- src/shader.cpp
+++ src/shader.cpp
@@ -2,30 +2,19 @@
 
 auto Shader::create_program_shader() {
 
+  // read shader source files
   std::string vertShaderStr = readShaderSource("shaders/vertShader.glsl");
   std::string fragShaderStr = readShaderSource("shaders/fragShader.glsl");
+
+  const char *vertShaderSrc = vertShaderStr.c_str();
+  const char *fragShaderSrc = fragShaderStr.c_str();
 
   // Create reference to access shaders
   GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
   GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-  const char *vertShaderSrc = vertShaderStr.c_str();
-  const char *fragShaderSrc = fragShaderStr.c_str();
-
   glShaderSource(vShader, 1, &vertShaderSrc, NULL);
   glShaderSource(fShader, 1, &fragShaderSrc, NULL);
-
-  // // Attach Vertex Shader source to the Vertex Shader Object, 1-> one screen
-  // glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-
-  // Compile the Vertex Shader into machine code for GPU
-  glCompileShader(vShader);
-
-  // // Attach Fragment Shader source to the Fragment Shader Object, 1-> one
-  // screen glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-
-  // Compile the Fragment Shader into machine code for GPU
-  glCompileShader(fShader);
 
   // create list of compiles shaders
   GLuint shaderProgram = glCreateProgram();
