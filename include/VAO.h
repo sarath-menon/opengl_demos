25094b7a55f36e2c3dcb2ad1e5ecec8d39e32cfa
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-08 12:37:52 +0530

    added organization files

---
+++ include/VAO.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "VBO.h"
+#include <glad/glad.h>
+
+class VAO {
+public:
+  // ID reference for the Vertex Array Object
+  GLuint ID;
+  // Constructor that generates a VAO ID
+  VAO();
+
+  // Links a VBO to the VAO using a certain layout
+  void LinkVBO(VBO &VBO, GLuint layout);
+  // Binds the VAO
+  void Bind();
+  // Unbinds the VAO
+  void Unbind();
+  // Deletes the VAO
+  void Delete();
+};
