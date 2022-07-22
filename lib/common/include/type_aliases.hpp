44fbbe949758ea0b212b27fbc79fc3a694429153
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-23 00:47:17 +0530

    fixed dynamics matrix size

--- lib/common/include/type_aliases.hpp
+++ lib/common/include/type_aliases.hpp
@@ -7,9 +7,12 @@ namespace gl {
 using V3 = Eigen::Vector3f;
 using V4 = Eigen::Vector4f;
 
-// matrix
+// static matrix
 using M3 = Eigen::Matrix<float, 3, 3, Eigen::RowMajor>;
 using M4 = Eigen::Matrix<float, 4, 4, Eigen::RowMajor>;
+
+// dynamic matrices
+using M3DC = Eigen::Matrix<float, 3, Eigen::Dynamic, Eigen::RowMajor>;
 
 using A3 = Eigen::Affine3f;
 
