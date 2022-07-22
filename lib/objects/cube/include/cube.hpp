2445612d41155f1e5cabc84c600aa4488c460f2b
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-22 19:52:07 +0530

    added cube module

--- lib/objects/cube/include/cube.hpp
+++ lib/objects/cube/include/cube.hpp
@@ -8,19 +8,16 @@
 #include <glm/glm.hpp>
 #include <string>
 
-class Triangle {
+class Cube {
 
 private:
-  gl::M3 vertices_{};
+  gl::M3 coord_{};
 
 public:
   // Equilateral triangle given starting coord and side length
-  Triangle(const gl::V3 start, const float length);
+  Cube(const gl::V3 coord, const float length);
 
-  // all 3 coordinates given
-  Triangle(const gl::V3 c1, const gl::V3 c2, const gl::V3 c3);
-
-  auto &vertices() { return vertices_; }
+  auto &vertices() { return coord_; }
 };
 
 // vertices << -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
