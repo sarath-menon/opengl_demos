fdc414f8f736c676b1d503100f7a102121d9751e
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-22 11:37:14 +0530

    updated cube_demo api

--- app/cube_demo/src/main.cpp
+++ app/cube_demo/src/main.cpp
@@ -1,75 +1,75 @@
 #include "VAO.hpp"
 #include "VBO.hpp"
-#include "glfw_helper.hpp"
 #include "shader.hpp"
 #include "stb_image.h"
 #include "triangle.hpp"
+#include "viewer.hpp"
 #include <Eigen/Dense>
 #include <unsupported/Eigen/OpenGLSupport>
 
-constexpr unsigned int width = 800;
-constexpr unsigned int height = 800;
-
 int main() {
 
-  GLFWHelper glfw_helper;
+  Viewer viewer(720, 480);
 
-  auto window = glfw_helper.get_window();
+  Shader shader("shaders/2d_vertShader.glsl", "shaders/2d_fragShader.glsl");
 
-  Shader shader("shaders/3d_vertShader.glsl", "shaders/3d_fragShader.glsl");
+  // create triangles
+  Triangle t1(gl::V3(-0.4, -0.4, 0), 1.0);
+  Triangle t2(gl::V3(0.4, 0.4, 0), 1.0);
 
-  Triangle t1(gl::V3(0, 0, 0), 1.0);
+  // vertex array object to prganize vertex buffers
+  VAO VA;
 
-  // Generates Vertex Array Object and binds it
-  VAO VAO1;
-  VAO1.Bind();
+  // vertex buffer to be sent to vertex shader
+  VBO VB[] = {VBO(t1.vertices()), VBO(t2.vertices())};
 
-  // Generates Vertex Buffer Object and links it to vertices
-  VBO VBO1(t1.vertices());
+  // Activate the VA
+  VA.Bind();
 
-  // Links VBO to VAO
-  VAO1.LinkAttrib(VBO1, 0, t1.vertices(), GL_FLOAT);
+  // Link VAO to VBO
+  VA.LinkAttrib(VB[0], 0, t1.vertices(), GL_FLOAT);
+  VA.LinkAttrib(VB[1], 1, t2.vertices(), GL_FLOAT);
 
   // Unbind all to prevent accidentally modifying them
-  VAO1.Unbind();
-  VBO1.Unbind();
+  VA.Unbind();
+  VB[0].Unbind();
+  VB[1].Unbind();
 
   // Gets ID of uniform called "scale"
-  GLuint uniID = glGetUniformLocation(shader.get_program(), "scale");
+  GLuint uniID = glGetUniformLocation(shader.getHandle(), "scale");
 
   //  Render loop: show window till close button is pressed
-  while (!glfwWindowShouldClose(window)) {
+  while (!glfwWindowShouldClose(viewer.getHandle())) {
     // Inputs
-    glfw_helper.processInput(window);
+    viewer.processInput();
 
     // Display
-    glfw_helper.display(shader, glfwGetTime());
+    viewer.display(shader, glfwGetTime());
 
     // Load the compiled shaders to the GPU
     shader.Activate();
 
     // Draw cube ////////////////////////////////
 
-    gl::M4 model = Eigen::Matrix4f::Identity();
-    gl::M4 view = Eigen::Matrix4f::Identity();
-    gl::M4 proj = Eigen::Matrix4f::Identity();
-
-    ////////////////////////////////
+    gl::M4 model = gl::M4::Identity();
+    gl::M4 view = gl::M4::Identity();
+    gl::M4 proj = gl::M4::Identity();
 
     // Bind the VAO so OpenGL knows to use it
-    VAO1.Bind();
+    VA.Bind();
 
-    // Start drawing-GLSL pipeline starts (primitive,start vertex, vertex count)
+    // send data in vertex buffer to the shader and start drawing
+    // (primitive,start vertex, vertex count)
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Process events~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     // Swap the back buffer with the front buffer
-    glfwSwapBuffers(window);
+    glfwSwapBuffers(viewer.getHandle());
     // handles window events, such as close button pressed
     glfwPollEvents();
   }
 
-  glfw_helper.terminate_window(shader);
+  viewer.terminate();
   return 0;
 }
