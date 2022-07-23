dd17120f76d60708d635037e7e4e8c733a593b5b
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-23 16:50:02 +0530

    added varying colour fature to fragment shader

--- shaders/3d_fragShader.glsl
+++ shaders/3d_fragShader.glsl
@@ -3,4 +3,11 @@
 // Outputs colors in RGBA
 out vec4 FragColor;
 
-void main() { FragColor = vec4(0.67f, 0.84f, 0.90f, 1.0f); }
+in vec4 varying_colour;
+
+void main() {
+  // single colour
+  FragColor = vec4(0.67f, 0.84f, 0.90f, 1.0f);
+  // varying colour
+  FragColor = varying_colour;
+}
--- shaders/3d_vertShader.glsl
+++ shaders/3d_vertShader.glsl
@@ -5,6 +5,10 @@ layout(location = 0) in vec3 pos;
 uniform mat4 modelview;
 uniform mat4 proj;
 
+out vec4 varying_colour;
+
 // opengl recognises as output, vertex position
-void main() { gl_Position = proj * modelview * vec4(pos, 1.0); }
-// void main() { gl_Position = vec4(pos, 1.0); }
+void main() {
+  gl_Position = proj * modelview * vec4(pos, 1.0);
+  varying_colour = vec4(pos, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+}
