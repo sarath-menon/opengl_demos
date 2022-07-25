5899ef6f8b2732037faf60ea89b7a77695e00b61
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-25 16:25:18 +0530

    added texture

--- shaders/3d_fragShader.glsl
+++ shaders/3d_fragShader.glsl
@@ -3,11 +3,10 @@
 // Outputs colors in RGBA
 out vec4 FragColor;
 
-in vec4 varying_colour;
+// Inputs the texture coordinates from the Vertex Shader
+in vec2 texCoord;
 
-void main() {
-  // single colour
-  FragColor = vec4(0.67f, 0.84f, 0.90f, 1.0f);
-  // varying colour
-  FragColor = varying_colour;
-}
+// Gets the Texture Unit from the main function
+uniform sampler2D tex0;
+
+void main() { FragColor = texture(tex0, texCoord); }
--- shaders/3d_vertShader.glsl
+++ shaders/3d_vertShader.glsl
@@ -1,14 +1,19 @@
 #version 410 core
-// take input pos of vec3 type at location 0
-layout(location = 0) in vec3 pos;
 
+// vertices
+layout(location = 0) in vec3 pos;
 uniform mat4 model;
 uniform mat4 cam_view;
 
-out vec4 varying_colour;
+// texture
+layout(location = 1) in vec2 aTex;
+// Outputs the texture coordinates to the fragment shader
+out vec2 texCoord;
 
 // opengl recognises as output, vertex position
 void main() {
   gl_Position = cam_view * model * vec4(pos, 1.0);
-  varying_colour = vec4(pos, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+
+  // for gragment shader
+  texCoord = aTex;
 }
