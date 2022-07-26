fe13da23e4ec63f18604ad2850f1508849a7f72b
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-26 12:48:51 +0530

    remove unnecessary headers

--- lib/common/include/gl_common.hpp
+++ lib/common/include/gl_common.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "custom_literals.hpp"
-#include "look_at.hpp"
 #include "math.hpp"
-#include "perspective.hpp"
 #include "type_aliases.hpp"
+
+// #include "perspective.hpp"
+// #include "look_at.hpp"
