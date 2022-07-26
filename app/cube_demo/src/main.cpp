4b90818c5e79b6453b1a3c8002284a7f7cccca97
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-26 11:54:42 +0530

    API update

--- app/cube_demo/src/main.cpp
+++ app/cube_demo/src/main.cpp
@@ -18,17 +18,15 @@ int main() {
   constexpr float width = 600;
   constexpr float height = 600;
 
-  constexpr float camera_fov = 60.0_deg;
   constexpr float cam_pos[3] = {0.0f, 0.0f, 10.0f};
-  constexpr float near_plane = 0.1f;
-  constexpr float far_plane = 100.0f;
-
-  constexpr float cube_pos[3] = {1.0, -2.0, -1.0};
+  const gl::V3 cube_pos = gl::V3(1.0, -2.0, -1.0);
+  const glm::vec4 obj_colour = glm::vec4(0.8f, 0.3f, 0.02f, 1.0f);
 
   Viewer viewer(width, height);
-  Shader shader("shaders/3d_vertShader.glsl", "shaders/3d_fragShader.glsl");
+  Shader obj_shader("shaders/3d_constcolour_vshader.glsl",
+                    "shaders/3d_constcolour_fshader.glsl");
   Camera camera(width, height, glm::vec3(glm::make_vec3(cam_pos)));
-  Cube cube(gl::V3(cube_pos[0], cube_pos[1], cube_pos[2]), 1.0);
+  Cube cube(cube_pos, 1.0);
 
   // to hold id's of uniform variables
   GLuint model_loc;
@@ -44,12 +42,19 @@ int main() {
   // Transformation matrices
   gl::A3 model_m = gl::A3::Identity();
   // get locations of uniforms in the shader program
-  model_loc = glGetUniformLocation(shader.getHandle(), "model");
+  model_loc = glGetUniformLocation(obj_shader.getHandle(), "model");
 
   // start timer
   t.start();
 
   cube.set_global_position(gl::V3(1.0f, 1.0f, 1.0f));
+
+  // Load the compiled shaders to the GPU
+  obj_shader.Activate();
+
+  // set object colour
+  glUniform4f(glGetUniformLocation(obj_shader.getHandle(), "obj_colour"),
+              obj_colour.x, obj_colour.y, obj_colour.z, obj_colour.w);
 
   //  Render loop: show window till close button is pressed
   while (!glfwWindowShouldClose(viewer.getHandle())) {
@@ -57,15 +62,14 @@ int main() {
     viewer.processInput();
     // clear display
     viewer.clear_display();
-    // Load the compiled shaders to the GPU
-    shader.Activate();
 
     // Camera ////////////////////////////////
+    // camera
     // Handles camera inputs
     camera.Inputs(viewer.getHandle());
 
-    // Updates and exports the camera matrix to the Vertex Shader
-    camera.Matrix(camera_fov, near_plane, far_plane, shader, "cam_view");
+    // Update the camera position according to mouse interaction
+    camera.updateMatrix();
 
     // Draw cube ////////////////////////////////
 
@@ -80,9 +84,12 @@ int main() {
     // Link vaO to vbO
     va.link_vertices(vb[0], 0, GL_FLOAT);
 
-    // Drawring/////////////
-    // send data in vertex buffer to the shader and start drawing
+    // send data in vertex buffer to the obj_shader and start drawing
     glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    //////////////////////////////////////////////////////////
+    // Export the camMatrix to the Vertex Shader of the pyramid
+    camera.Matrix(obj_shader, "cam_view");
     viewer.start_display();
   }
 }
