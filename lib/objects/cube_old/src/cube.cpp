7cfadeeb88da3bef633d2428d347430786c24a54
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-27 20:14:02 +0530

    added indices

---
+++ lib/objects/cube_old/src/cube.cpp
@@ -0,0 +1,24 @@
+#include "cube.hpp"
+#include <iostream>
+
+Cube::Cube(const gl::V3 coord, const float length) {
+
+  global_pose_ = gl::A3::Identity();
+  global_pose_.translation() = coord;
+
+  length_ = length;
+
+  // set vertices
+  vertices_ = Eigen::Matrix3Xf::Zero(3, 36);
+
+  vertices_ << -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+      1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f,
+      -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+      -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+      -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+      -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,
+      -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f;
+}
--- lib/objects/pyramid/src/pyramid.cpp
+++ lib/objects/pyramid/src/pyramid.cpp
@@ -9,17 +9,28 @@ Pyramid::Pyramid(const gl::V3 coord, const float length) {
   length_ = length;
 
   // set vertices and texture coordinates
-  vertices_ = Eigen::Matrix3Xf::Zero(3, 18);
-  texture_coord_ = Eigen::Matrix2Xf::Zero(2, 18);
+  vertices_ = Eigen::Matrix3Xf::Zero(3, 16);
 
-  vertices_ << -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-      -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, -1.0f, -1.0f,
-      -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,
-      1.0f, 0.0f, 1.0f, 0.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f,
-      -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f;
+  vertices_ << -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f, -0.5f, 0.5f,
+      0.0f, 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, 0.0f, 0.8f, 0.0f,
+      -0.5f, 0.0f, -0.5f, 0.5f, 0.0f, -0.5f, 0.0f, 0.8f, 0.0f, 0.5f, 0.0f,
+      -0.5f, 0.5f, 0.0f, 0.5f, 0.0f, 0.8f, 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
+      0.5f, 0.0f, 0.8f, 0.0f;
 
-  texture_coord_ << 0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-      0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-      0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-      1.0f, 0.0f;
+  // set texture coordinates
+  texture_coord_ = Eigen::Matrix2Xf::Zero(2, 16);
+
+  texture_coord_ << 0.0f, 0.0f, 0.0f, 5.0f, 5.0f, 5.0f, 5.0f, 0.0f, 0.0f, 0.0f,
+      5.0f, 0.0f, 2.5f, 5.0f, 2.5f, 5.0f, 0.0f, 0.0f, 2.5f, 5.0f, 0.0f, 0.0f,
+      5.0f, 0.0f, 2.5f, 5.0f, 5.0f, 0.0f, 0.0f, 0.0f, 2.5f, 5.0f;
+
+  //  set indices
+  indices_ = Eigen::Matrix3Xi::Zero(3, 6);
+
+  indices_ << 0, 1, 2, // Bottom side
+      0, 2, 3,         // Bottom side
+      4, 6, 5,         // Left side
+      7, 9, 8,         // Non-facing side
+      10, 12, 11,      // Right side
+      13, 15, 14;      // Facing side;
 }
