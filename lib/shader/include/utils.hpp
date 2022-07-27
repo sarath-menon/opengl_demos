34069497ef460ae03e3e082dd2a8dd79fc0a4157
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-28 00:31:20 +0530

    removed unnecessary dependencies

--- lib/shader/include/utils.hpp
+++ lib/shader/include/utils.hpp
@@ -1,12 +1,7 @@
 #include <glad/glad.h>
-//
-#include <GLFW/glfw3.h>
-//#include <SOIL2/SOIL2.h>
+
 #include <cmath>
 #include <fstream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
