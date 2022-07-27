30a05a104e0c2798fc041ea05575bec758dbc282
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-27 20:35:47 +0530

    first working version

--- app/multiple_objects/src/main.cpp
+++ app/multiple_objects/src/main.cpp
@@ -1,3 +1,4 @@
+#include "EBO.hpp"
 #include "VAO.hpp"
 #include "VBO.hpp"
 #include "camera.hpp"
@@ -6,6 +7,7 @@
 #include "shader.hpp"
 #include "stb_image.h"
 #include "timer.hpp"
+#include "triangle.hpp"
 #include "viewer.hpp"
 #include <iostream>
 
@@ -18,37 +20,53 @@ int main() {
   constexpr float width = 600;
   constexpr float height = 600;
 
-  constexpr float cam_pos[3] = {0.0f, 2.0f, 10.0f};
-
+  constexpr float cam_pos[3] = {0.0f, 0.0f, 5.0f};
   const gl::V3 cube_pos = gl::V3(1.0, -2.0, -1.0);
   const gl::V3 pyramid_pos = gl::V3(0.0, 0.0, 0.0);
   const glm::vec4 obj_colour = glm::vec4(0.8f, 0.3f, 0.02f, 1.0f);
 
-  Viewer viewer(width, height, "Multiple objects");
+  Viewer viewer(width, height, "Cube with vertices drawn using indices");
   Shader obj_shader("shaders/3d_constcolour_vshader.glsl",
                     "shaders/3d_constcolour_fshader.glsl");
   Camera camera(width, height, glm::vec3(glm::make_vec3(cam_pos)));
+
   Cube cube(cube_pos, 1.0);
   Pyramid pyramid(pyramid_pos, 1.0);
 
   // to hold id's of uniform variables
   GLuint model_loc;
 
-  // vertex array object to prganize vertex buffers
-  VAO va;
+  cube.set_scale(0.5f);
 
+  // vertex array object to prganize vertex buffers
+  VAO cube_va;
   // vertex buffer to be sent to vertex shader
-  VBO vb[2];
-  vb[0].set_vertices(cube.vertices());
-  vb[1].set_vertices(pyramid.vertices());
+  VBO cube_vb[2];
+  cube_vb[0].set_vertices(cube.vertices());
+  // Generates Element Buffer Object and links it to indices
+  EBO cube_eb(cube.indices());
+  cube_va.Bind();
+  // Link VBO to  VAO
+  cube_va.set_vertex_attrb_ptrs(cube_vb[0], 0, GL_FLOAT);
+  // cube_eb.Unbind();
+  cube_va.Unbind();
+
+  // // vertex array object to prganize vertex buffers
+  // VAO pyramid_va;
+  // // vertex buffer to be sent to vertex shader
+  // VBO pyramid_vb[2];
+  // pyramid_vb[0].set_vertices(pyramid.vertices());
+  // // Generates Element Buffer Object and links it to indices
+  // EBO pyramid_eb(pyramid.indices());
+  // pyramid_eb.Unbind();
+  // pyramid_va.Unbind();
 
   // Transformation matrices
   gl::A3 model_m = gl::A3::Identity();
-
   // get locations of uniforms in the shader program
   model_loc = glGetUniformLocation(obj_shader.getHandle(), "model");
 
-  cube.set_global_position(gl::V3(1.0f, 3.0f, 1.0f));
+  cube.set_global_position(gl::V3(1.0f, 1.0f, 1.0f));
 
   // Load the compiled shaders to the GPU
   obj_shader.Activate();
@@ -57,13 +75,16 @@ int main() {
   glUniform4f(glGetUniformLocation(obj_shader.getHandle(), "obj_colour"),
               obj_colour.x, obj_colour.y, obj_colour.z, obj_colour.w);
 
+  cube_va.Bind();
+
   //  Render loop: show window till close button is pressed
   while (!glfwWindowShouldClose(viewer.getHandle())) {
     // Inputs
     viewer.processInput();
-
+    // clear display
     viewer.clear_display();
 
+    // Camera ////////////////////////////////
     // camera
     // Handles camera inputs
     camera.Inputs(viewer.getHandle());
@@ -71,24 +92,8 @@ int main() {
     // Update the camera position according to mouse interaction
     camera.updateMatrix();
 
-    // Draw pyramid ////////////////////////////////
-    // build view,model matrices
-    pyramid.global_rotate_x(M_PI / 100.0f);
+    // Draw cube ////////////////////////////////
 
-    // create model matrix
-    model_m = pyramid.global_pose();
-
-    // copy matrix data to corresponding uniform variables
-    glUniformMatrix4fv(model_loc, 1, GL_FALSE, model_m.data());
-
-    // Link vaO to vbO
-    va.set_vertex_attrb_ptrs(vb[1], 0, GL_FLOAT);
-
-    // draw
-    glDrawArrays(GL_TRIANGLES, 0, 18);
-
-    //////////////////////////////////////////
-    // Draw cube
     cube.global_rotate_y(M_PI / 100.0f);
 
     // create model matrix
@@ -97,21 +102,12 @@ int main() {
     // copy matrix data to corresponding uniform variables
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model_m.data());
 
-    // Link vaO to vbO
-    va.set_vertex_attrb_ptrs(vb[0], 0, GL_FLOAT);
-
-    // enable wireframe drawing
-    viewer.enable_wireframe_view();
-
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-
-    // Disable wireframe drawing
-    viewer.enable_solid_view();
+    // send data in vertex buffer to the obj_shader and start drawing
+    glDrawElements(GL_TRIANGLES, cube.indices().size(), GL_UNSIGNED_INT, 0);
 
     //////////////////////////////////////////////////////////
     // Export the camMatrix to the Vertex Shader of the pyramid
     camera.Matrix(obj_shader, "cam_view");
-
     viewer.start_display();
   }
 }
