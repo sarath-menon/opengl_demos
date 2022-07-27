bfae5dfd48c3f6476cd612cf0e442233987fa4f2
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-27 22:17:54 +0530

    fixed vertex,  texture corrdinates

--- lib/objects/pyramid/src/pyramid.cpp
+++ lib/objects/pyramid/src/pyramid.cpp
@@ -9,28 +9,18 @@ Pyramid::Pyramid(const gl::V3 coord, const float length) {
   length_ = length;
 
   // set vertices and texture coordinates
-  vertices_ = Eigen::Matrix3Xf::Zero(3, 16);
+  vertices_ = gl::M3DR::Zero(3, 5);
 
   vertices_ << -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f, -0.5f, 0.5f,
-      0.0f, 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, 0.0f, 0.8f, 0.0f,
-      -0.5f, 0.0f, -0.5f, 0.5f, 0.0f, -0.5f, 0.0f, 0.8f, 0.0f, 0.5f, 0.0f,
-      -0.5f, 0.5f, 0.0f, 0.5f, 0.0f, 0.8f, 0.0f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
-      0.5f, 0.0f, 0.8f, 0.0f;
+      0.0f, 0.5f, 0.0f, 0.8f, 0.0f;
 
   // set texture coordinates
-  texture_coord_ = Eigen::Matrix2Xf::Zero(2, 16);
+  texture_coord_ = gl::M2DR ::Zero(2, 5);
 
-  texture_coord_ << 0.0f, 0.0f, 0.0f, 5.0f, 5.0f, 5.0f, 5.0f, 0.0f, 0.0f, 0.0f,
-      5.0f, 0.0f, 2.5f, 5.0f, 2.5f, 5.0f, 0.0f, 0.0f, 2.5f, 5.0f, 0.0f, 0.0f,
-      5.0f, 0.0f, 2.5f, 5.0f, 5.0f, 0.0f, 0.0f, 0.0f, 2.5f, 5.0f;
+  texture_coord_ << 0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 5.0f, 0.0f, 2.5f, 5.0f;
 
   //  set indices
   indices_ = Eigen::Matrix3Xi::Zero(3, 6);
 
-  indices_ << 0, 1, 2, // Bottom side
-      0, 2, 3,         // Bottom side
-      4, 6, 5,         // Left side
-      7, 9, 8,         // Non-facing side
-      10, 12, 11,      // Right side
-      13, 15, 14;      // Facing side;
+  indices_ << 0, 1, 2, 0, 2, 3, 0, 1, 4, 1, 2, 4, 2, 3, 4, 3, 0, 4;
 }
