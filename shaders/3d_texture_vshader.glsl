46785c8ef1b745900e7fe19892fb6a892ca1108d
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-28 19:45:44 +0530

    added colours

--- shaders/3d_texture_vshader.glsl
+++ shaders/3d_texture_vshader.glsl
@@ -6,7 +6,12 @@ uniform mat4 model;
 uniform mat4 cam_view;
 
 // texture
-layout(location = 1) in vec2 aTex;
+layout(location = 1) in vec2 acolor;
+// Outputs the texture coordinates to the fragment shader
+out vec2 colours;
+
+// texture
+layout(location = 2) in vec2 aTex;
 // Outputs the texture coordinates to the fragment shader
 out vec2 texCoord;
 
