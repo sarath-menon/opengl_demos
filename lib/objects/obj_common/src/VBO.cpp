aed25d72d7e3238136242a0aa88883e12980754f
sarath-menon <sarathmenon.downey@gmail.com>
2022-07-28 20:04:36 +0530

    code cleanup

--- lib/objects/obj_common/src/VBO.cpp
+++ lib/objects/obj_common/src/VBO.cpp
@@ -4,14 +4,7 @@
 // Constructor that generates a Vertex Buffer Object and links it to vertices
 VBO::VBO() { VBO::create_one_buffer(); }
 
-void VBO::set_vertices(const gl::M3DR &M) const {
-
-  VBO::Bind();
-
-  // We expect a matrix with each vertex position on a row, we then want to
-  // pass this data to OpenGL reading across rows (row-major)
-
-  /* copy vertex data to the active vertex buffer
+/* copy vertex data to the active vertex buffer
    API: (buffer type, data size, data, usage)
    usage -> tell the graphics card how to manage the data
 
@@ -20,6 +13,11 @@ void VBO::set_vertices(const gl::M3DR &M) const {
    GL_STATIC_DRAW: the data is set only once and used many times.
    GL_DYNAMIC_DRAW: the data is changed a lot and used many times.*/
 
+void VBO::set_vertices(const gl::M3DR &M) const {
+  VBO::Bind();
+
+  // We expect a matrix with each vertex position on a row, we then want to
+  // pass this data to OpenGL reading across rows (row-major)
   glBufferData(GL_ARRAY_BUFFER, sizeof(float) * M.size(), M.data(),
                GL_STATIC_DRAW);
 
@@ -27,12 +25,10 @@ void VBO::set_vertices(const gl::M3DR &M) const {
 }
 
 void VBO::set_colours(const gl::M3DR &M) const {
-
   VBO::Bind();
 
   // We expect a matrix with one (R,G,B) colour for one vertex in a row, we then
   // want to pass this data to OpenGL reading across rows (row-major)
-
   glBufferData(GL_ARRAY_BUFFER, sizeof(float) * M.size(), M.data(),
                GL_STATIC_DRAW);
 
@@ -40,12 +36,10 @@ void VBO::set_colours(const gl::M3DR &M) const {
 }
 
 void VBO::set_texture(const gl::M2DR &M) const {
-  this->Bind();
+  VBO::Bind();
 
   // We expect a matrix with each texture coordinate pair in a row, we then want
   // to pass this data to OpenGL reading across rows (row-major)
-
-  // copy vertices to the active vertex buffer
   glBufferData(GL_ARRAY_BUFFER, sizeof(float) * M.size(), M.data(),
                GL_STATIC_DRAW);
 
